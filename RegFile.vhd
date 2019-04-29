8f559f1bf370f4ddc6987d2942c9acd254d1fb5e
RussReding <36208413+RussReding@users.noreply.github.com>
2019-04-28 17:32:25 -0700

    Add files via upload

--- RegFile.vhd
+++ RegFile.vhd
@@ -1,51 +1,35 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+------------------------------------------------------
+-- Register Memory Block
 -- 
--- Create Date: 04/23/2019 10:49:28 AM
--- Design Name: 
--- Module Name: RegFile - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- Contains all the registers.
 -- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+-- Memory is kept in rows of 32 bits to represent 32-bit
+-- registers.
+------------------------------------------------------
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;  -- NEED TO USE THE COMPONENTS WE HAVE BUILT INSTEAD OF LIB-------------------
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-entity RegFile is
-  Port  ( reg1        : in  STD_LOGIC_VECTOR (4 downto 0);  -- takes instr 25-21
-          reg2        : in  STD_LOGIC_VECTOR (4 downto 0);  -- takes instr 20-16
-          write_reg   : in  STD_LOGIC_VECTOR (4 downto 0);  -- to write to reg to mem unit
-          write_data  : in  STD_LOGIC_VECTOR (31 downto 0); -- 
-          write_en    : in  STD_LOGIC;                      -- enables 'write_reg' and 'write_data'
-          clk         : in std_logic;                       -- clock (write_en is checked on rising clock edge)
-          
-          data1       : out STD_LOGIC_VECTOR (31 downto 0); -- used for adder 'A'
-          data2       : out STD_LOGIC_VECTOR (31 downto 0) -- adder 'B' or save to mem
-          
-          );
+entity RegFile is 
+	port (
+        ck: in std_logic;
+		write_enabled: in std_logic;
+		read_reg_1: in std_logic_vector(4 downto 0);
+		read_reg_2: in std_logic_vector(4 downto 0);
+		write_reg: in std_logic_vector(4 downto 0);
+		write_data: in std_logic_vector(31 downto 0);
+		read_data_1: out std_logic_vector(31 downto 0);
+		read_data_2: out std_logic_vector(31 downto 0)
+	);
 end RegFile;
 
+architecture Behavorial of RegFile is
 
-
-
-architecture Behavioral of RegFile is
-    -- setting up reg block of 32 registers each with 32 bits. each is init with '0..0'
-    type reg_a is array (0 to 31) of 
-    STD_LOGIC_VECTOR (31 downto 0);
-    signal reg_block : reg_a :=(
-        "00000000000000000000000000000000", 
+    -- 128 byte register memory (32 rows * 4 bytes/row)
+    type mem_array is array(0 to 31) of STD_LOGIC_VECTOR (31 downto 0);
+    signal reg_mem: mem_array := (
+        "00000000000000000000000000000000",
         "00000000000000000000000000000000", 
         "00000000000000000000000000000000",
         "00000000000000000000000000000000",
@@ -65,31 +49,29 @@ architecture Behavioral of RegFile is
         "00000000000000000000000000000000",
         "00000000000000000000000000000000",
         "00000000000000000000000000000000",  
-        "00000000000000000000000000000000", 
-        "00000000000000000000000000000000",
-        "00000000000000000000000000000000",
-        "00000000000000000000000000000000",
-        "00000000000000000000000000000000", 
         "00000000000000000000000000000000",
         "00000000000000000000000000000000",
         "00000000000000000000000000000000",
         "00000000000000000000000000000000",
         "00000000000000000000000000000000", 
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
+        "00000000000000000000000000000000",
         "00000000000000000000000000000000", 
+        "00000000000000000000000000000000",
         "00000000000000000000000000000000"
     );
-begin
-    data1 <= reg_block(to_integer(unsigned(reg1)));
-    data2 <= reg_block(to_integer(unsigned(reg2)));
 
-    process(clk)
+	begin
+
+    read_data_1 <= reg_mem(to_integer(unsigned(read_reg_1)));
+    read_data_2 <= reg_mem(to_integer(unsigned(read_reg_2)));
+
+    process(ck)
         begin
-        -- write to mem reg is rising clk edge and write is enabled
-        if(clk = '1')then
-          if (clk'event = '1') then
-          if (write_en = '1') then
-            reg_block(to_integer(unsigned(write_reg))) <= write_data;
+        if ck='0' and ck'event and write_enabled='1' then
+            reg_mem(to_integer(unsigned(write_reg))) <= write_data;
         end if;
     end process;
-end process;
-end Behavioral;
+end Behavorial;
